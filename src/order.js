25ed357c9ea6e12912ba4b4402403574fb940ff2
Michann Stoner <stoner.michann@gmail.com>
2021-02-06 17:23:51 -0700

    Add order.js and order-test.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,47 @@
+function takeOrder(orders, deliveryArray) {
+  if (deliveryArray.length < 3) {
+    deliveryArray.push(orders);
+    return deliveryArray;
+  }else{
+    return deliveryArray;
+  }
+};
+
+function refundOrder(orderNumber, deliveryOrders) {
+  deliveryOrders.splice(orderNumber - 1, 1);
+    return deliveryOrders;
+};
+
+function listItems(deliveryOrders) {
+  var items = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item);
+  }
+  return items.join(', ');
+};
+
+function searchOrder(deliveryOrders, item) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === item) {
+      return true;
+  }else if (deliveryOrders[1].item === item) {
+      return true;
+  }else{
+      return false;
+    }
+  }
+};
+// Need to refactor this to not be hard coded but feeling very stuck!!
+
+
+
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
